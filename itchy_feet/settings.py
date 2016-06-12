dc84c1cfddc2307f8ec23947e3fa4838083127c2
rockyshiba <n01145868@humbermail.ca>
2016-06-12 01:31:40 -0400

    Register model
    Register model completed

--- itchy_feet/settings.py
+++ itchy_feet/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'about.apps.AboutConfig',
+    'register.apps.RegisterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- register/models.py
+++ register/models.py
@@ -3,5 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Traveller(models.Model):
-    first_name = models.CharField(max_length= 50)
-    last_name = models.CharField(max_length= 50)
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    GENDERS = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length = 1, choices = GENDERS)
+    email = models.EmailField(max_length = 100)
+    hometown = models.CharField(max_length = 200)
+    biography = models.TextField(null = True, blank = True, help_text = "Don't write your life story! Just a little bit")
+    image = models.ImageField(upload_to = None, height_field = None, width_field = None, max_length=100)
+    date_of_birth = models.DateField()
+    created_at = models.DateTimeField(auto_now = False, auto_now_add = True)
+    edited_at = models.DateTimeField(auto_now = True, auto_now_add = False)
+    deleted_at = models.DateTimeField(auto_now = False, auto_now_add = True)
+    terms = models.BooleanField()
+
